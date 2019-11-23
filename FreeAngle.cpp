c90c54c4354acba44d8d730375f04d01e5ae0968
Victor <avieira.victor@gmail.com>
2019-11-22 21:59:32 -0300

    9obstaculos

--- FreeAngle.cpp
+++ FreeAngle.cpp
@@ -11,10 +11,10 @@ float distancia(int x1, int y1, int x2, int y2){                //calc dist entr
     return p;
 }
 int main (){
-    int x,y,xm,ym,xm2,ym2,d,a,e,f=0,g=0,h,i,b=0,t=1,aux;
+    int x,y,xm,ym,xm2,ym2,d,a,e,f=0,g=0,h,i,b=0,t=1,j=0,q=0,w=0,r=0,o=0,aux;
     float dist,m,ang,d1,d2,dist2,kt,d3,d4,d5;
-    float ponto[4][4];
-    int adv[4][2];
+    float ponto[9][4];
+    int adv[9][2];
     int bolax,bolay,jogx,jogy,batata,cLateral2y,cLateral2x,cLateraly,cLateralx;
 
     sf::RenderWindow window(sf::VideoMode(930, 630), "Path Planning");
@@ -73,6 +73,31 @@ int main (){
     circle7.setPosition(110,300);
     circle7.setOutlineColor(sf::Color::Black);
     circle7.setOutlineThickness(3);
+    sf::CircleShape circle8(15);
+    circle8.setFillColor(sf::Color::Blue);
+    circle8.setPosition(345,400);
+    circle8.setOutlineColor(sf::Color::Black);
+    circle8.setOutlineThickness(3);
+    sf::CircleShape circle9(15);
+    circle9.setFillColor(sf::Color::Blue);
+    circle9.setPosition(300,400);
+    circle9.setOutlineColor(sf::Color::Black);
+    circle9.setOutlineThickness(3);
+    sf::CircleShape circle10(15);
+    circle10.setFillColor(sf::Color::Blue);
+    circle10.setPosition(255,400);
+    circle10.setOutlineColor(sf::Color::Black);
+    circle10.setOutlineThickness(3);
+    sf::CircleShape circle11(15);
+    circle11.setFillColor(sf::Color::Blue);
+    circle11.setPosition(210,400);
+    circle11.setOutlineColor(sf::Color::Black);
+    circle11.setOutlineThickness(3);
+    sf::CircleShape circle12(15);
+    circle12.setFillColor(sf::Color::Blue);
+    circle12.setPosition(110,400);
+    circle12.setOutlineColor(sf::Color::Black);
+    circle12.setOutlineThickness(3);
     while(window.isOpen()){
         bolax= bola.getPosition().x + 5;
         bolay= bola.getPosition().y + 5;
@@ -86,6 +111,18 @@ int main (){
         adv[2][1]=circle5.getPosition().y + 15;
         adv[3][0]=circle6.getPosition().x + 15;
         adv[3][1]=circle6.getPosition().y + 15;
+        adv[4][0]=circle7.getPosition().x + 15;
+        adv[4][1]=circle7.getPosition().y + 15;
+        adv[5][0]=circle8.getPosition().x + 15;
+        adv[5][1]=circle8.getPosition().y + 15;
+        adv[6][0]=circle9.getPosition().x + 15;
+        adv[6][1]=circle9.getPosition().y + 15;
+        adv[7][0]=circle10.getPosition().x + 15;
+        adv[7][1]=circle10.getPosition().y + 15;
+        adv[8][0]=circle11.getPosition().x + 15;
+        adv[8][1]=circle11.getPosition().y + 15;
+
+        
         sf::Event event;
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed){
@@ -106,6 +143,17 @@ int main (){
                 if((x>=adv[3][0] - 15  && x<=adv[3][0] + 15)&&(y>=adv[3][1] - 15 && y<=adv[3][1] + 15))
                     b=1;
 
+                if((x>=adv[4][0] - 15  && x<=adv[4][0] + 15)&&(y>=adv[4][1] - 15 && y<=adv[4][1] + 15))
+                    j=1;
+                if((x>=adv[5][0] - 15  && x<=adv[5][0] + 15)&&(y>=adv[5][1] - 15 && y<=adv[5][1] + 15))
+                    q=1;
+                if((x>=adv[6][0] - 15  && x<=adv[6][0] + 15)&&(y>=adv[6][1] - 15 && y<=adv[6][1] + 15))
+                    w=1;
+                if((x>=adv[7][0] - 15  && x<=adv[7][0] + 15)&&(y>=adv[7][1] - 15 && y<=adv[7][1] + 15))
+                    r=1;
+                if((x>=adv[8][0] - 15  && x<=adv[8][0] + 15)&&(y>=adv[8][1] - 15 && y<=adv[8][1] + 15))
+                    o=1;
+               
             }
             if(event.type == sf::Event::MouseButtonReleased){
                 d=2;
@@ -114,7 +162,13 @@ int main (){
                 g=2;
                 h=2;
                 b=2;
-                i=2;
+                j=2;
+                q=2;
+                w=2;
+                r=2;
+                o=2;
+            
+
             }
             if (event.type == sf::Event::MouseMoved)
             {
@@ -134,22 +188,33 @@ int main (){
             circle5.setPosition(x-15,y-15);
         if(b==1)
             circle6.setPosition(x-15,y-15);
+        if(j==1)
+            circle7.setPosition(x-15,y-15);
+        if(q==1)
+            circle8.setPosition(x-15,y-15);
+        if(w==1)
+            circle9.setPosition(x-15,y-15);
+        if(r==1)
+            circle10.setPosition(x-15,y-15);
+        if(o==1)
+            circle11.setPosition(x-15,y-15);
 
         //FREE ANGLE
-        for(i=0;i<4;i++){
+        for(i=0;i<9;i++){
             dist = distancia(adv[i][0],adv[i][1], jogx, jogy);
             float dist2 = sqrt(pow(dist, 2) + pow(18, 2));
 
             if(adv[i][0]-jogx!=0)
                 m = float(adv[i][1]-jogy)/float(adv[i][0]-jogx);
 
-            else {
-                if (adv[i][1] > jogy) m = 90;                       //Adv em cima
-                else m = -90;                                       //Adv em baixo
-            }
-
+            
             float alfa = asin(18/dist);
             float beta = atan(m);
+
+            if(adv[i][0]-jogx==0){
+                if (adv[i][1] > jogy) beta = 1.5708;                       //Adv em cima
+                else beta = -1.5708;
+            }
 
             if(jogx > adv[i][0])
                 batata=-1;
@@ -163,14 +228,9 @@ int main (){
             ponto[i][2] = jogx + int(batata*dist2*cos(beta-alfa));
             ponto[i][3] = jogy + int(batata*dist2*sin(beta-alfa));
 
-            if(jogx == adv[i][0]){
-                ponto[i][0] = jogx + int(batata*dist2*cos(beta+alfa));
-                ponto[i][1] = jogy - int(batata*dist2*sin(beta+alfa));
-
-                ponto[i][2] = jogx - int(batata*dist2*cos(beta-alfa));
-                ponto[i][3] = jogy + int(batata*dist2*sin(beta-alfa));
-            }
+            
         }
+
 
         window.clear();
         window.draw(rectangle);
@@ -178,7 +238,7 @@ int main (){
         window.draw(circle);
         window.draw(lines);
 
-        for(i=0;i<4;i++){
+        for(i=0;i<9;i++){
             sf::VertexArray lateral(sf::Triangles, 3);
             lateral[0].position=sf::Vector2f(ponto[i][0], ponto[i][1]);
             lateral[1].position = sf::Vector2f(jogx, jogy);
@@ -188,12 +248,18 @@ int main (){
             lateral[2].color = sf::Color::Blue;
             window.draw(lateral);
         }
-
         window.draw(circle3);
         window.draw(circle4);
         window.draw(circle5);
         window.draw(circle2);
         window.draw(circle6);
+        window.draw(circle8);
+        window.draw(circle9);
+        window.draw(circle10);
+        window.draw(circle11);
+        window.draw(circle7);
         window.display();
+
+        }
     }
-}
+
